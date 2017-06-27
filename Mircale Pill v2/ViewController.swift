bc1344fe2088d4c479b05568a61e02a646f01c0b
Nao <naofumikudo@gmail.com>
2017-06-27 04:48:41 -0700

    first commit

--- Mircale Pill v2/ViewController.swift
+++ Mircale Pill v2/ViewController.swift
@@ -8,10 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var stateList: UIPickerView!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    
+    let states = ["Alaska","Alabama","California","Florida","Maine","New York"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        stateList.dataSource = self
+        stateList.delegate = self
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,6 +28,29 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func stateBtnPicker(_ sender: Any) {
+        
+        stateList.isHidden = false
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal)
+        stateList.isHidden = true
+    }
+    
 
 
 }
